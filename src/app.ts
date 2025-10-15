cb7d70e8403dcad73560d82a489c92fb87a7012b
shiwangi-1009 <shiwangi.upadhyay@shelfexecution.com>
2025-10-16 01:39:09 +0530

    Cors Error Solved(Allows Patch Request)

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:3000", // your frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     credentials: true,
   })
 );
