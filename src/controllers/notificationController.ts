4fbed9fbc0ddcc78555ec04c3679d0d3e98a5a69
sydshelf75 <sridhar.sing@shelfexecution.com>
2025-10-30 12:30:04 +0530

    create a notification controller which handle send notification and get notification

---
+++ src/controllers/notificationController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import NotificationService from "../services/NotificationService";
+import Notification from "../models/notification";
+
+export class NotificationController {
+    static async sendNotification(req: Request, res: Response) {
+        try {
+            const { userId, title, message, metadata } = req.body;
+
+            const notification = await NotificationService.sendNotification({
+                userId,
+                title,
+                message,
+                metadata
+            })
+
+
+            return res.json({ success: true, notification })
+        } catch (error: any) {
+            return res.status(500).json({ success: false, error: error.message })
+        }
+    }
+
+    static async getNotification(req: Request, res: Response) {
+        try {
+            const { userId } = req.params;
+            const { page = 1, limit = 20 } = req.query
+
+            const notifications = await Notification.find({ userId }).sort({ created: -1 }).skip((Number(page) - 1) * Number(limit)).limit(Number(limit));
+
+            return res.json({ success: true, notifications })
+        } catch (error: any) {
+            return res.status(500).json({ success: false, error: error.message })
+        }
+    }
+}
